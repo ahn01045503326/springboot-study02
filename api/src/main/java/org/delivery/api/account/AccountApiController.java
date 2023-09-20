a451e9b77e420988c628e405654b8bbd22db9a97
AHN <ahn01045503326@gmail.com>
2023-09-21 01:11:42 +0900

    API 기본설정(ObjectMapper config파일 설정)

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,14 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +17,14 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save() {
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("me")
+    public AccountMeResponse me() {
+
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("A@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
+
 }
---
+++ api/src/main/java/org/delivery/api/config/objectmapper/ObjectMapperConfig.java
@@ -0,0 +1,27 @@
+package org.delivery.api.config.objectmapper;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new Jdk8Module()); // jdk 8 버전 이 클래스
+        objectMapper.registerModule(new JavaTimeModule()); // << local date
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 모르는 json field 대해서는 무시한다.
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 날짜 관련 직렬화
+        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy()); // 스네이크케이스
+
+        return objectMapper;
+    }
+}
