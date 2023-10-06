e7455bfb1b69c15dcfa515683a70f6b0e2838ca2
AHN <ahn01045503326@gmail.com>
2023-10-07 01:50:50 +0900

    JWT 토큰 서비스 로직

---
+++ api/src/main/java/org/delivery/api/common/error/TokenErrorCode.java
@@ -0,0 +1,24 @@
+package org.delivery.api.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * user의 경우 2000번대 에러코드 사용
+ */
+@AllArgsConstructor
+@Getter
+public enum TokenErrorCode implements ErrorCodeIfs {
+
+    INVALID_TOKEN(400, 2000, "유효하지 않은 토큰"),
+    EXPIRED_TOKEN(400, 2001, "만료된 토큰"),
+    TOKEN_EXCEPTION(400,2002, "토큰 알 수 없는 에러")
+    ;
+
+    private final Integer httpStatusCode;
+
+    private final Integer errorCode;
+
+    private final String description;
+
+}
--- api/src/main/java/org/delivery/api/domain/token/helper/JwtTokenHelper.java
+++ api/src/main/java/org/delivery/api/domain/token/helper/JwtTokenHelper.java
@@ -1,10 +1,22 @@
 package org.delivery.api.domain.token.helper;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.TokenErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.api.domain.token.ifs.TokenHelperIfs;
 import org.delivery.api.domain.token.model.TokenDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -18,16 +30,77 @@ public class JwtTokenHelper implements TokenHelperIfs {
     private Long refreshTokenPlusHour;
     @Override
     public TokenDto issueAccessToken(Map<String, Object> data) {
-        return null;
+        var expiredLocalDateTime = LocalDateTime.now().plusHours(accessTokenPlusHour);
+
+        var expiredAt = Date.from(
+            expiredLocalDateTime.atZone(
+                ZoneId.systemDefault()
+            ).toInstant()
+        );
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        var jwtToken = Jwts.builder()
+            .signWith(key, SignatureAlgorithm.HS256)
+            .setClaims(data)
+            .setExpiration(expiredAt)
+            .compact();
+
+        return TokenDto.builder()
+            .token(jwtToken)
+            .expiredAt(expiredLocalDateTime)
+            .build();
     }
 
     @Override
     public TokenDto issueRefreshToken(Map<String, Object> data) {
-        return null;
+        var expiredLocalDateTime = LocalDateTime.now().plusHours(refreshTokenPlusHour);
+
+        var expiredAt = Date.from(
+            expiredLocalDateTime.atZone(
+                ZoneId.systemDefault()
+            ).toInstant()
+        );
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        var jwtToken = Jwts.builder()
+            .signWith(key, SignatureAlgorithm.HS256)
+            .setClaims(data)
+            .setExpiration(expiredAt)
+            .compact();
+
+        return TokenDto.builder()
+            .token(jwtToken)
+            .expiredAt(expiredLocalDateTime)
+            .build();
     }
 
     @Override
     public Map<String, Object> validationTokenWithThrow(String token) {
-        return null;
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        var parser = Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build();
+
+        try {
+            var result = parser.parseClaimsJws(token);
+            return new HashMap<String, Object>(result.getBody());
+
+        } catch(Exception e) {
+
+            if(e instanceof SignatureException) {
+                // 토큰이 유효하지 않을 때
+                throw new ApiException(TokenErrorCode.INVALID_TOKEN);
+            } else if(e instanceof ExpiredJwtException) {
+                // 만료된 토큰
+                throw new ApiException(TokenErrorCode.EXPIRED_TOKEN);
+            } else {
+                // 그외 에러
+                throw new ApiException(TokenErrorCode.TOKEN_EXCEPTION);
+
+            }
+        }
     }
 }
