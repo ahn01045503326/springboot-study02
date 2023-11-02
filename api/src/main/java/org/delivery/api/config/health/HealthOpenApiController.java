f47874e0602f7a68c323d978e6fbaf2ef1afade0
AHN <ahn01045503326@gmail.com>
2023-11-02 23:12:28 +0900

    rabbitmq 설정 (exchange, queue)

---
+++ api/src/main/java/org/delivery/api/config/health/HealthOpenApiController.java
@@ -0,0 +1,23 @@
+package org.delivery.api.config.health;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.delivery.api.common.rabbitmq.Producer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/open-api")
+public class HealthOpenApiController {
+
+    private final Producer producer;
+
+    @GetMapping("/health")
+    public void health() {
+        log.info("health call");
+        producer.producer("delivery.exchange", "delivery.key", "hello");
+    }
+}
